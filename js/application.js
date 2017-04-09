db49deb4cae3adaf5e74040076b5da07badb6e57
Justine Dupuis <justine.dps26@gmail.com>
2017-04-09 23:33:16 +0200

    initial commit — css issues (paths modif needed)

---
+++ js/application.js
@@ -0,0 +1,11 @@
+//= require jquery
+//= require jquery.turbolinks
+//= require jquery_ujs
+//= require turbolinks
+//= require materialize-sprockets
+//= require materialize
+//= require_tree .
+
+$(document).ready(function(){
+    $('.collapsible').collapsible();
+  });
